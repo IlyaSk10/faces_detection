09921e6077f34eff6aa5b4b1fa82e90aa59e8046
IlyaSk10 <50613848+IlyaSk10@users.noreply.github.com>
2020-08-26 12:02:24 +0300

    Update temp.py

--- preprocessing/temp.py
+++ preprocessing/temp.py
@@ -14,6 +14,20 @@ def load_face_dataset(path):
 
 	return dataset, target
 
+
+def normalize(dataset):
+	std = np.std(dataset.reshape(dataset.shape[0], -1), axis=1)
+	mean = np.mean(dataset.reshape(dataset.shape[0], -1), axis=1)
+	for i in range(len(std)):
+		dataset[i] = (dataset[i] - mean[i]) / std[i]
+	return dataset
+
+
 def pickle_it(data, path):
 	with open(path, 'wb') as f:
 		pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def unpickle_it(path):
+	with open(path, 'rb') as f:
+		return pickle.load(f)
