2f6eb7abb445e412d1600f0bef5aab8a125920f7
IlyaSk10 <50613848+IlyaSk10@users.noreply.github.com>
2020-08-26 11:58:44 +0300

    Update temp.py

--- preprocessing/temp.py
+++ preprocessing/temp.py
@@ -3,6 +3,17 @@ from os import walk
 import cv2
 import numpy as np
 
+
+
+def load_face_dataset(path):
+	face = load_dir(path + 'face\\')
+	non_face = load_dir(path + 'non-face\\')
+
+	target = np.hstack((np.ones(len(face)), np.zeros(len(non_face))))
+	dataset = np.asarray(face + non_face)
+
+	return dataset, target
+
 def pickle_it(data, path):
 	with open(path, 'wb') as f:
 		pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
